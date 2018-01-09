ac5fb7e7d07a9e222bbf63ab2211ad7d37dd48e9
hengyi <hengyicai@hotmail.com>
2018-01-09 21:42:51 +0800

    Add example.

--- seq2seq.sh
+++ seq2seq.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-#CUDA_VISIBLE_DEVICES=""
+# CUDA_VISIBLE_DEVICES=""
 
 /usr/bin/env python3 -m translate "$@"
+
+# Running example
+#   Train a model: ./seq2seq.sh config/default.yaml --train -v
